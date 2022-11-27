251216df96a905e4600d602d30058dee16d72d84
DespinSR <100006284+DespinSR@users.noreply.github.com>
2022-11-26 21:16:02 -0600

    Create episode6.m

---
+++ _game/shines/bianco/episode6.m
@@ -0,0 +1,147 @@
+---
+layout: default 
+title: Bianco 6
+permalink: /shines/bianco/episode6/
+parent: Bianco Hills
+grand_parent: Shines
+---
+# Bianco 6
+Bianco 6 is made up of two parts: the Entry and the Secret. You should be comfortable with hoverslides, (jump) dive rollouts, spin jumps, sideflips, and triple jumps to have success with this shine. The intermediate and advanced strats for this level require knowledge of glitchy wall kick dives (GWKDs) and insta-momentum spin jumps.
+## Entry
+The entry strats will be similar for most skill levels, except for some small optimizations, and the one major strat difference: Two Slide vs One Slide.
+### Two Slide (Normal)
+- From the start, hold up and hoverslide
+- Aim for the middle of the log, buffer a hover as you slide off the path, release hover, then double jump off the log
+- If your double jump doesn’t have enough speed to reach the doorway, do a short hover to land just inside the doorway.
+- Hoverslide (or just dive) rollout on the right side.
+- Aiming just left of the sign with the arrow, jump and spam spray, then dive into the water and start a waterslide
+- Slide partway up the hill and rollout on the left side, then jump spam spray dive to get your waterslide going.
+- After the bridge, rollout over the gap in the path, dive rollout towards the small grass hill, then jump between the green shrubs. A short hover is safe here but a tad slower than just landing.
+- Hoverslide along the path, holding up right to get you going in the right direction and still preserve speed. 
+- Delay your hover, but not so much that you're too close to the water and have to hover a long time. Ideal is a height where you only have to hover about 1 second and will be at a good position to dive onto the log.
+- Rollout onto the left log, and it's your choice if you want to do a momentum jump + hover dive, or a bit safer is a spinjump to gain more height, then hover dive
+
+{% include yt.html id="picRx1Yapv8" %}
+### One Slide (Advanced)
+- This is very precise and requires a lot of practice and precise control stick adjustment. I'd probably recommend this if you have a 1:17 or better, give or take a little depending on how much you like grinding precise strats.
+- To get one-slide, the most important thing to do is preserve a lot of speed going up the slope. To do this, you need to hit a very specific part of the slope:
+- https://i.imgur.com/HfKWpkH.jpg
+- From there, precise control stick input is required to not bonk on the pianta or the left wall.
+- If you can tell you have a bad angle, just bail early and rollout for two slide.
+- One-slide saves around a second or maybe a bit more, depending on what you consider a "true" two-slide vs. a failed one-slide that is backed up into two-slide. 
+
+{% include yt.html id="je66MezEBu8" %}
+## Secret
+This secret is boils down to three sections, the first cube grab, the cube skip, and the upper section. The cycles on the upper section become harder the slower you do the earlier sections. 
+For this reason, you should look for strats that suit your skill level and optimize them to be able to reach your desired cycle at the top.
+
+### Cube Grab
+#### Old Cube Grab
+Almost everyone uses this strat and I think it's hard and slow. Here it is for reference.
+- dive, rollout, jump dive, re-dive, rollout, dive, rollout, spinjump dive, slide a bit, then rollout onto the cube.
+{% include yt.html id="hSI5Y4GvtzE" %}
+
+#### New Cube Grab
+This strat has higher potential for speed, and I think it's easier. I'm not sure how hard a beginner would find it, but I think it should be manageable.
+It typically saves me around 0.2 over the old approach, and I think it's perfectly viable for RTA.
+
+If you watch the example video, you might notice the 3rd rollout looks really tight, as mario would probably slide off the edge in the next few frames if I didn't rollout.
+You can make this more lenient by shortening the first 2 rollouts. At absolute fastest, you would use a 5f and a 2f rollout, but this leaves you basically no room for error. Something like 3f + 2f rollout is safer and is still fairly fast.
+Here's the breakdown:
+- Dive, rollout (2-4f to be safe), dive, rollout (1-2f), dive, rollout (full), jump dive, rollout up left to reach the main platform
+- Jump dive (careful of the star here, if you're a bit too far forward or a bit slower, you can bonk on it, so maybe delay your dive a bit)
+- Rollout onto cube. For most strats, you want to grab around the intersection of the blue and white areas. For speedspin (see Advanced section), you would want to grab in the middle of the cube. 
+
+{% include yt.html id="jjEXwrTelwI" %}
+
+### Strat Selection to Ensure Good Top Cycle
+As I mentioned above, the easier cycles at the top are the earlier ones. So the faster you go on the cube skip, the easier the top is. 
+But how should you pick your strats? What is each strat capable of? Here's a chart:
+
+| Cube Skip             | No GWKD                                     | GWKD                              |
+| 2nd rotation mom spin | Blue cycle                                  | Left Side (hard)                  |
+| Spin + Y              | Left Side (somewhat hard)                   | Left Side (easy) or UTM (hard)    |
+| Rollout Spin + Y      | Left Side (slightly easier than Spin+Y)     | Up the Middle or UTM (kinda hard) |
+| Speedspin             | Left Side (trivial) or Up the Middle (hard) | Up the Middle (medium)            |
+
+This is gonna depend highly on how much you optimize your movement in the first half, and how much you optimize either your GWKD or your spinjump at the top.
+
+### Cube Skip
+### 2nd Rotation Momentum Spin (Beginner)
+#### Best Cube Trigger - 14.10 (not actually a very realistic time to hit)
+- When you grab the cube, center your camera with L and press A to get up.
+- Wait for the second turn of the cube when it rotates backwards. Wait until the cube turns enough to become a roughly 45 degree angle slope.
+- Perform a short jump, then dive into the cube and perform an insta-momentum spin jump all the way to the yellow platform.
+- Dive rollout on the yellow platform, then dive rollout again onto the elevator cube
+- Hold neutral on the stick and press B when Mario lands on the cube to make him grab the air. This immediately stops his momentum; the cube waits for Mario to be at a complete stop to begin traveling.
+
+{% include yt.html id="hSI5Y4GvtzE" %}
+### Spinjump Y-Turn (Beginner-Intermediate)
+#### Best Cube Trigger - 13.57 (not too much better than average)
+- Run forward, jump, then buffered spin jump off the edge of the cube. You want to make sure you get good speed on this jump. (i.e. you've buffered the spinput enough to where you can walk a few frames before jumping)
+- Hold forward for a bit, then do a small y-turn towards the yellow wall. Mario’s angle approaching the wall should be a little less than 45 degrees.
+-- Play with the angle on this, it's more lenient to get a wall kick than you think.
+- Exit Y-cam before you hit the wall. Wall kick + dive off the wall towards the yellow platform.
+- Rollout, dive, rollout, and you should land on the cube. Hold neutral and press B to stop Mario’s momentum.
+{% include yt.html id="jjEXwrTelwI" %}
+### Rollout Spinjump Y-turn (Intermediate-Advanced)
+#### Best Cube Trigger - 13.23 (little bit better than average)
+- This is the same idea as Spin + Y, but you rollout on the cube to get some momentum for the spinjump. You can get around 40 speed if done well.
+- You want to delay the rollout until the platform will be at a good slope when you land. You can do this too early and preserve no rollout speed.
+- This strat is somewhat limited by the doritos. If you want to maximize the timesave of this strat, you'll need to rollout and let the first dorito grab you.
+- From there, you need to safely get off the dorito. Best case, one dive will get you down safely, but sometimes the dive will get eaten and you'll land back on it immediately.
+- For that reason, I recommend mashing B to dive repeatedly, which should cover for the cases where the first dive fails. Usually the second dive works fine.
+- You could also jump, then delayed dive off the dorito. This might have the same issue, not sure.
+{% include yt.html id="Psq0REdZISs" %}
+### Speedspin (Advanced)
+#### Best Cube Trigger - 12.97 (it's very easy to get close to this time)
+- Ledgegrab the middle of the cube. This can work from the right side, but it's more difficult, and not necessary, because this strat forces you to wait for the dorito cycle anyway.
+- Hold up left to go all the way to the edge of the cube, then hold up right, small jump, delayed dive, spinput, A to rollout, then delayed A press to ensure you momentum spin in the right direction.
+- Dive rollout to the right side. If you're early enough, you need to wait for the second dorito to get out of the way. 
+- Time your next dive according to when you know the dorito will be gone, then short rollout onto the cube.
+
+{% include yt.html id="iv6_41aPYWU" %}
+## Upper Secret
+Save state practice is highly recommended for this section.
+### General No-GWKD Tips
+- The best way to get up to the top platform is a buffered spinjump. The easiest way to do this is time a short jump on the back of the cube, then walk forward with your spinjump.
+- Try to find a timing queue for when to do your jump. Unfortunately there's no obvious visual queue, so you might try counting for a second after the cube stops rotating, or something similar.
+### General GWKD Tips
+- There's 2 methods for this GWKD, straight-forward, or angled. An angled GWKD allows you to do the GWKD at a lower (and thus earlier) point on the wall, and thus it can save time generally.
+- The slight disadvantage is that you will need to turn to go forward, so you will lose some distance compared to the straight-on method. To make up for this, generally you want to include another dive rollout where ordinarily you might jumpdive to the red plat.
+- Another caveat to angled GWKD is that you will bonk if you get a "frame perfect" GWKD. Basically this happens if you press Y before you hit the wall. This causes the angle reflection to not happen, so you just go the exact angle you approached the wall at, which will make you bonk here.
+- For the timing, what I like to do is a loose buffered spinjump (walk in a circle), and I'll begin the spin as soon as the cube stops rotating, or even a bit before if I'm playing really aggro.
+
+### Blue Cycle (AKA Cycleless)
+#### Difficulty - Medium-Hard
+- Spin jump straight up then dive rollout.
+- Spinjump dive to the blue platform on the right.
+- Perform a short rollout (hold back while you rollout for safety)
+- Spinjump dive from here to the next blue platform. Rollout onto the red plat.
+- Dive rollout, dive rollout, then jumpdive into the Shine.
+
+{% include yt.html id="hSI5Y4GvtzE" %}
+### Left Side
+#### Difficulty - Easy
+No GWKD:
+- Spin jump off the cube, then dive rollout.
+- Jump dive to the red plat ahead.
+- Rollout onto the blue plat, then spinjump dive to the next blue plat.
+- Rollout onto the red plat, then jump dive (or spinjump dive for safety) to green platform. Rollowut, then jump dive into the shine. If you did a spinjump dive, try to do a small rollout before the jump dive into the shine.
+{% include yt.html id="jjEXwrTelwI" %}
+GWKD:
+- Only difference is you will jump dive instead of the second dive rollout on the yellow plat, then double dive on the first red plat.
+{% include yt.html id="C-OLM675WHA" %}
+### Up the Middle
+#### Difficulty - depends how early you get there (earlier is easier)
+GWKD: 
+- GWKD onto the yellow platform, rollout, then jump dive to the red plat.
+- Eat some dust, then rollout to the next red plat.
+- Dive, then rollout to the blue plat.
+- OPTIMALLY - Dive rollout of the blue square and onto the red plat.
+- IF YOU'RE LATE - you may be able to jump dive to the top of the red plat and rollout from it. Definitely want to make sure you know the distance to do it from, though. Pretty easy to bonk if you misjudge it.
+- Jump dive to the green platform, rollout, and jump dive into the shine.
+Optimal ending:
+{% include yt.html id="qW2i6vDQtB4" %}
+Late ending:
+{% include yt.html id="igxiTsu0qKc" %}
